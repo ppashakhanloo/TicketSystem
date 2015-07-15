7a80cb0f94aa229eff58eed50c754d698e016cc6
Pardis <ppashakhanloo@gmail.com>
2015-07-15 23:20:37 +0430

    Toman added in submit-new-event. view-event-details comming soon...

---
+++ scripts/pcustom.js
@@ -0,0 +1,9 @@
+// This event is triggered when the star rating is modified or changed.
+// This event also allows you to access these parameters:
+// value: the selected rating value
+// caption: the caption for the selected rating
+$('#inputRate').on('rating.change', function(event, value, caption) {
+    console.log(value);
+    console.log(caption);
+    // we'll use this for saving rate and ...
+});
